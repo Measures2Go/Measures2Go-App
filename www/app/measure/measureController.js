6e51e529ed03ce5f50bac43c3f54434c179115db
Dillon Buchanan <thedillonb@gmail.com>
2015-09-07 12:16:34 -0400

    Now evaluating results

--- www/app/measure/measureController.js
+++ www/app/measure/measureController.js
@@ -3,5 +3,26 @@ angular.module('measures2go')
   Measure.getTest($stateParams.id, function(err, test) {
     $scope.title = test.name;
     $scope.data = test.content;
+    $scope.values = [];
+
+    $scope.totalValue = function() {
+      var v = 0;
+      angular.forEach($scope.values, function(x) {
+        v += x * 1;
+      })
+      return v;
+    };
+
+    $scope.result = function() {
+      var x = $scope.totalValue();
+      console.log(test.results);
+      for (var i = 0; i < test.results.length; i++) {
+        if (eval(test.results[i].eval)) {
+          return test.results[i].result;
+        }
+      }
+
+      return '';
+    }
   })
 });
